e5a18f8007a60395ad7b806bf9c7998c8a3ef4f6
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-07-12 22:00:51 -0500

    firebase

--- utils/upload.util.js
+++ utils/upload.util.js
@@ -1,14 +1,18 @@
 const multer = require("multer")
+const path = require("path")
 
-const storage = multer.diskStorage({
+/* const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "/imgs")
+        cb(null, path.join(__dirname, "..", "imgs"))
     },
     filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, "test.png")
+        
+        const filename = `${Date.now()}_${file.originalname}`
+        cb(null, filename)
     }
-})
+}) */
+
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage})
 
